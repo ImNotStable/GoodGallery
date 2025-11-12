7e979b33df23af534b324a172b0235dd267b55b0
Jeremiah <jeremiahhughes06@gmail.com>
2025-11-12 09:43:51 -0600

    Update Gradle configuration: Reorder Lombok dependency and enable jar task while disabling shadowJar task for better build management.

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -1,11 +1,20 @@
 dependencies {
   implementation("org.jetbrains:annotations:26.0.2-1")
-  implementation("org.projectlombok:lombok:1.18.42")
   annotationProcessor("org.jetbrains:annotations:26.0.2-1")
+  implementation("org.projectlombok:lombok:1.18.42")
   annotationProcessor("org.projectlombok:lombok:1.18.42")
 
   //implementation("com.zaxxer:HikariCP:7.0.2")
   //implementation("org.xerial:sqlite-jdbc:3.50.3.0")
   implementation("com.google.guava:guava:33.5.0-jre")
   implementation("com.google.code.gson:gson:2.13.2")
+}
+
+tasks {
+  jar {
+    enabled = true
+  }
+  shadowJar {
+    enabled = false
+  }
 }
